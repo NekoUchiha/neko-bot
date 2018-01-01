6ea82721497819c99a84f2ad1c0b42e0deb9b5c0
NekoUchiha <34886772+NekoUchiha@users.noreply.github.com>
2018-01-01 20:12:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,99 +1,199 @@
-const Discord=require('discord.js');
-const YTDL = require("ytdl-core");
-const robot = new Discord.Client();
-const { TOKEN, PREFIX } = require('./config');
+const { Client, Util } = require('discord.js');
+const { TOKEN, PREFIX, GOOGLE_API_KEY } = require('./config');
+const YouTube = require('simple-youtube-api');
+const ytdl = require('ytdl-core');
 
-function generateHex() {
-    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+const client = new Client({ disableEveryone: true });
+
+const youtube = new YouTube(GOOGLE_API_KEY);
+
+const queue = new Map();
+
+client.on('warn', console.warn);
+
+client.on('error', console.error);
+
+client.on('ready', () => console.log(`Logged in as ${client.user.username}!`));
+
+client.on('disconnect', () => console.log('I just disconnected, making sure you know, I will reconnect now...'));
+
+client.on('reconnecting', () => console.log('I am reconnecting now!'));
+
+client.on('message', async msg => { // eslint-disable-line
+	if (msg.author.bot) return undefined;
+	if (!msg.content.startsWith(PREFIX)) return undefined;
+
+	const args = msg.content.split(' ');
+	const searchString = args.slice(1).join(' ');
+	const url = args[1] ? args[1].replace(/<(.+)>/g, '$1') : '';
+	const serverQueue = queue.get(msg.guild.id);
+
+	if (msg.content.startsWith('neko play')) {
+		const voiceChannel = msg.member.voiceChannel;
+		if (!voiceChannel) return msg.channel.send('Nya. onegai connecto voice chanal');
+		const permissions = voiceChannel.permissionsFor(msg.client.user);
+		if (!permissions.has('CONNECT')) {
+			return msg.channel.send('Watashi wa ii connecto wa channal :(');
+		}
+		if (!permissions.has('SPEAK')) {
+			return msg.channel.send('Watashi wa chan`neru de hanasu koto ga dekimasen');
+		}
+
+		if (url.match(/^https?:\/\/(www.youtube.com|youtube.com)\/playlist(.*)$/)) {
+			const playlist = await youtube.getPlaylist(url);
+			const videos = await playlist.getVideos();
+			for (const video of Object.values(videos)) {
+				const video2 = await youtube.getVideoByID(video.id); // eslint-disable-line no-await-in-loop
+				await handleVideo(video2, msg, voiceChannel, true); // eslint-disable-line no-await-in-loop
+			}
+			return msg.channel.send(`âœ… Playlist: **${playlist.title}** Nya. addedo to the queue!`);
+		} else {
+			try {
+				var video = await youtube.getVideo(url);
+			} catch (error) {
+				try {
+					var videos = await youtube.searchVideos(searchString, 10);
+					let index = 0;
+					msg.channel.send(`
+**Song selection:**
+
+${videos.map(video2 => `**${++index} -** ${video2.title}`).join('\n')}
+
+Please provide a value to select one of the search results ranging from 1-10.
+					`);
+					// eslint-disable-next-line max-depth
+					try {
+						var response = await msg.channel.awaitMessages(msg2 => msg2.content > 0 && msg2.content < 11, {
+							maxMatches: 1,
+							time: 20000,
+							errors: ['time']
+						});
+					} catch (err) {
+						console.error(err);
+						return msg.channel.send('No or invalid value entered, cancelling video selection.');
+					}
+					const videoIndex = parseInt(response.first().content);
+					var video = await youtube.getVideoByID(videos[videoIndex - 1].id);
+				} catch (err) {
+					console.error(err);
+					return msg.channel.send('ðŸ†˜ I could not obtain any search results.');
+				}
+			}
+			return handleVideo(video, msg, voiceChannel);
+		}
+	} else if (msg.content.startsWith('neko skip')) {
+		if (!msg.member.voiceChannel) return msg.channel.send('Nya. onegai connecto voice chanal');
+		if (!serverQueue) return msg.channel.send('There is nothing playing that I could skip for you.');
+		serverQueue.connection.dispatcher.end('Nya. Skip commando usa!');
+		return undefined;
+	} else if (msg.content.startsWith('neko stop')) {
+		if (!msg.member.voiceChannel) return msg.channel.send('Nya. onegai connecto voice chanal');
+		if (!serverQueue) return msg.channel.send('There is nothing playing that I could stop for you.');
+		serverQueue.songs = [];
+		serverQueue.connection.dispatcher.end('Nya. Stopo commando usede!');
+		return undefined;
+	} else if (msg.content.startsWith('neko volume')) {
+		if (!msg.member.voiceChannel) return msg.channel.send('Nya. onegai connecto voice chanal');
+		if (!serverQueue) return msg.channel.send('There is nothing playing.');
+		if (!args[1]) return msg.channel.send(`The current volume is: **${serverQueue.volume}**`);
+		serverQueue.volume = args[1];
+		serverQueue.connection.dispatcher.setVolumeLogarithmic(args[1] / 5);
+		return msg.channel.send(`I set the volume to: **${args[1]}**`);
+	} else if (msg.content.startsWith('neko np')) {
+		if (!serverQueue) return msg.channel.send('There is nothing playing.');
+		return msg.channel.send(`ðŸŽ¶ Now playing: **${serverQueue.songs[0].title}**`);
+	} else if (msg.content.startsWith('neko queue')) {
+		if (!serverQueue) return msg.channel.send('There is nothing playing.');
+		return msg.channel.send(`
+__**Song queue:**__
+
+${serverQueue.songs.map(song => `**-** ${song.title}`).join('\n')}
+
+**Now playing:** ${serverQueue.songs[0].title}
+		`);
+	} else if (msg.content.startsWith('neko pause')) {
+		if (serverQueue && serverQueue.playing) {
+			serverQueue.playing = false;
+			serverQueue.connection.dispatcher.pause();
+			return msg.channel.send('â¸ Paused the music for you!');
+		}
+		return msg.channel.send('There is nothing playing.');
+	} else if (msg.content.startsWith('neko resume')) {
+		if (serverQueue && !serverQueue.playing) {
+			serverQueue.playing = true;
+			serverQueue.connection.dispatcher.resume();
+			return msg.channel.send('â–¶ Resumed the music for you!');
+		}
+		return msg.channel.send('There is nothing playing.');
+	} else if (msg.content.startsWith('neko ping')) {
+		msg.channel.send(`\`${Date.now() - message.createdTimestamp} ms\``);
+	  } else if (msg.content.startsWith('neko test')) {
+		msg.channel.send('Nya. Watashi wa neko desu! :cat: ');
+	  }
+
+	  return undefined;
+});
+
+async function handleVideo(video, msg, voiceChannel, playlist = false) {
+	const serverQueue = queue.get(msg.guild.id);
+	console.log(video);
+	const song = {
+		id: video.id,
+		title: Util.escapeMarkdown(video.title),
+		url: `https://www.youtube.com/watch?v=${video.id}`
+	};
+	if (!serverQueue) {
+		const queueConstruct = {
+			textChannel: msg.channel,
+			voiceChannel: voiceChannel,
+			connection: null,
+			songs: [],
+			volume: 5,
+			playing: true
+		};
+		queue.set(msg.guild.id, queueConstruct);
+
+		queueConstruct.songs.push(song);
+
+		try {
+			var connection = await voiceChannel.join();
+			queueConstruct.connection = connection;
+			play(msg.guild, queueConstruct.songs[0]);
+		} catch (error) {
+			console.error(`I could not join the voice channel: ${error}`);
+			queue.delete(msg.guild.id);
+			return msg.channel.send(`I could not join the voice channel: ${error}`);
+		}
+	} else {
+		serverQueue.songs.push(song);
+		console.log(serverQueue.songs);
+		if (playlist) return undefined;
+		else return msg.channel.send(`âœ… **${song.title}** has been added to the queue!`);
+	}
+	return undefined;
 }
 
-function play(content, message){
-    var server = servers[message.guild.id];
+function play(guild, song) {
+	const serverQueue = queue.get(guild.id);
 
-    server.dipatcher = connection.playStream(YTDL(server.queque[0], {filter: "audioonly"}));
+	if (!song) {
+		serverQueue.voiceChannel.leave();
+		queue.delete(guild.id);
+		return;
+	}
+	console.log(serverQueue.songs);
 
-    server.queque.shift();
+	const dispatcher = serverQueue.connection.playStream(ytdl(song.url))
+		.on('end', reason => {
+			if (reason === 'Stream is not generating quickly enough.') console.log('Song ended.');
+			else console.log(reason);
+			serverQueue.songs.shift();
+			play(guild, serverQueue.songs[0]);
+		})
+		.on('error', error => console.error(error));
+	dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
 
-    server.dipatcher.on("end", function(){
-        if (server.queque[0]) play(connection, message);
-        else connection.diconnect();
-    })
+	serverQueue.textChannel.send(`ðŸŽ¶ Nya. Starto playito: **${song.title}**`);
 }
 
-var bot = new Discord.Client();
-
-var servers = {};
-
-bot.on("ready",function(){
-    console.log("Ready");
-});
-
-
-bot.on("gulidMemberAdd", function(member) {
-    member.guild.channels.find("name", "general").sendMessage(member.toString() + " Konnichewa onegai Guesto");
-
-    member.addRole(Guesto);
-
-    member.guild.createRole({
-        name: member.user.username,
-        colore: generateHex(),
-        permissions: []
-    }).then(function(role) {
-        member.addRole(role);
-    });
-});
-
-bot.on("message", function(message) {
-    if (message.author.equals(bot.user)) return;
-
-    if (!message.content.startsWith(PREFIX)) return;
-
-    var args = message.content.substring(PREFIX.length).split(" ");
-
-    switch (args[0].toLowerCase()){
-        case "test":
-        message.channel.sendMessage("Nya. Watashi wa neko desu! :cat: ");
-        break;
-        case "removerole":
-        message.member.removeRole(Gmember.guild.roles.find("name", "Guesto"));
-        break;
-        case "deleterole":
-        message.member.guild.roles.find("name", "Guesto");
-        case "play":
-        if (!args[1]){
-            message.channel.sendMessage("Nya onegai musik linko");
-            return;
-        }
-
-        if (!message.member.voiceChannel){
-            message.channel.sendMessage("Nya. onegai connecto voice chanal");
-            return;
-        }
-
-        if (!servers[message.guild.id]) servers[message.guild.id] = {
-            queque: []
-        };
-
-        var server = servers[message.guild.id];
-        
-        server.queque.push(args[1]);
-
-        if (!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection){
-            play(connection, message);
-        });
-        break;
-        case "skip":
-        var server = servers[message.guild.id];
-
-        if (server.dipatcher) server.dipatcher.end();
-        break;
-        case "stop":
-        var server = servers[message.guild.id];
-        if (message.guild.voiceConnection) message.guild.voiceConnection.disconnect();
-        break;
-        default:
-        message.channel.sendMessage("Io no sono desu :cat: ");
-    }
-});
-
-bot.login(TOKEN);
+client.login(TOKEN);
