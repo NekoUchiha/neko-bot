82fb33eb70fa194167d3adad3ad441af378fafa1
NekoUchiha <34886772+NekoUchiha@users.noreply.github.com>
2018-01-16 03:17:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -424,7 +424,7 @@ app.use(express.static(__dirname + '/public'));
 // set the home page route
 app.get('/', (request, response) => {
     // ejs render automatically looks in the views folder
-    response.render('index');
+    response.render('bot');
 });
 
 app.listen(port, () => {
