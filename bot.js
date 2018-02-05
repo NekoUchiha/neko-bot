1aefb785dc11a407c0ae3662ab8837545e46ca7b
NekoUchiha <34886772+NekoUchiha@users.noreply.github.com>
2018-02-05 17:37:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -588,7 +588,6 @@ ${serverQueue.songs.map(song => `**-** ${song.title}`).join('\n')}
 }
 });
 }
-});
 
 async function handleVideo(video, msg, voiceChannel, playlist = false) {
 	const serverQueue = queue.get(msg.guild.id);
