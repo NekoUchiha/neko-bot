c4ab610e47ca9eb9411ef845b9ae48a4b5c6d58d
NekoUchiha <34886772+NekoUchiha@users.noreply.github.com>
2018-02-22 02:58:05 +0300

    test

--- bot.js
+++ bot.js
@@ -566,7 +566,7 @@ tu pingu: ${Math.round(client.ping)} ms`);
 			msg.delete();
 			setTimeout(function() {
 			const AnonsRole = msg.guild.roles.find("name", "Anonser");
-			if (!msg.member.roles.has(AnonsRole.id))return msg.channel.send({embed: {
+			if (!msg.member.roles.has(AnonsRole.id)) msg.channel.send({embed: {
 			"description": "------------------------------------------------",
 			"color": 15337994,
 			"timestamp": new Date(),
@@ -584,7 +584,7 @@ tu pingu: ${Math.round(client.ping)} ms`);
 				},
 			],
 	}
-	});	
+	}) return;
 				msg.channel.send({embed: {
 						"description": "-------------------------------------------------------------",
 						"color": 1693449,
