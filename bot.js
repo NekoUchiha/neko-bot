13b6d3eb81eb0765377207783b855ab77faad873
NekoUchiha <34886772+NekoUchiha@users.noreply.github.com>
2018-03-03 06:17:37 +0300

    new

--- bot.js
+++ bot.js
@@ -1087,13 +1087,16 @@ client.login(process.env.TOKEN);
 const http = require('http');
 const express = require('express');
 const app = express();
+var ejs = require('ejs');
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
 const port = process.env.PORT || 5000;
 
 // set the view engine to ejs
-app.set('view engine', 'ejs');
+app.set('views', __dirname + '/public');
+app.engine('html', ejs.renderFile);
+app.set('view engine', 'html');
 
 // make express look in the `public` directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
@@ -1101,7 +1104,7 @@ app.use(express.static(__dirname + '/public'));
 // set the home page route
 app.get('/', (request, response) => {
     // ejs render automatically looks in the views folder
-    response.render('bot');
+    response.render('public/Index.html');
 });
 
 app.listen(port, () => {
